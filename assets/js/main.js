c92c99c3bd5af968e1967a20e3e7ff0d34b4a5cf
mattactually <mattactually@gmail.com>
2018-01-30 11:50:03 -0800

    Added sum functions maybe it'll work Idk

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,163 +1,129 @@
 //Adapted from procesData.js from lecture 4
 
-   // use local file
-   //var url ="http://www.sfu.ca/~labuchan/IAT_355/data/beers.csv";
-   var url ="./assets/data/beers.csv";
+var url ="./assets/data/beers.csv";
+
+d3.csv(url,function (data){
+  console.log(" The number of Ale Beers is "+countAle);
+});
+
+//Updated text fields for diplaying selected dimensions and outputs of queries
+var selectedText = document.getElementById("SelectedText");
+var output = document.getElementById("Output");
+var sum = document.getElementById("sum");
+
+//Selected Dimension for use with d3 queries
+var dimension = "";
+
+
+function UpdateSelected(update, name){
+ selectedText.innerHTML = update;
+ dimension = name;
+ getSum();
+}
+
+function UpdateOutput(info){
+ output.innerHTML = info;
+}
+
+//Button listeners for each of the selectable dimensions
+document.getElementById("Selector-ID").addEventListener("click", function(){
+UpdateSelected("ID", "id");
+});
+document.getElementById("Selector-ABV").addEventListener("click", function(){
+UpdateSelected("ABV", "abv");
+});
+document.getElementById("Selector-IBU").addEventListener("click", function(){
+UpdateSelected("IBU", "ibu");
+});
+document.getElementById("Selector-Name").addEventListener("click", function(){
+UpdateSelected("Name", "name");
+});
+document.getElementById("Selector-Style").addEventListener("click", function(){
+UpdateSelected("Style", "style");
+});
+document.getElementById("Selector-Brewery").addEventListener("click", function(){
+UpdateSelected("Brewery", "brewery_id");
+});
+document.getElementById("Selector-Ounces").addEventListener("click", function(){
+UpdateSelected("Ounces", "ounces");
+});
+
+function getSum() {
+  var sum;
+  d3.csv(url, function (data) {
+    if (dimension != "abv"
+      || dimension != "ibu"
+      || dimension != "ounces") {
+      sum = "Data not quantitative -- sum not applicable";
+    } else {
+      data.forEach(function(d) {
+        d[dimension] = parseFloat(d[dimension]);
+      });
+      sum = d3.sum(data, function(d) {
+        return d[dimension];
+      });
+    }
+    updateSum(sum);
+  });
+}
+
+function updateSum(info) {
+  sum.innerHTML = info;
+}
+
+function FindMax(){
+ console.log("It's Working (Max)");
 
  d3.csv(url,function (data){
 
-     //   console.log(" # Beers in dataset:  "+data.length)
-     // // filter data to get only  price less than 500
+   //https://stackoverflow.com/questions/30874617/d3-max-didnt-get-the-correct-value
+   data.forEach(function(d) {
+     //Need to parse float so that the max function doesn't order based on string value
+        d[dimension] = parseFloat(d[dimension]);
+    });
+   var m=d3.max(data,function (d){
+      return d[dimension];
+    });
 
-     // var americanIPA= data.filter(function (d){
+    console.log(m);
+    UpdateOutput(m);
+ });
+}
 
-     //     return (d.style == "American IPA")
-     // });
+function FindMin(){
+ console.log("It's Working (Min)");
 
-        // console.log(" # American IPAs:  " + americanIPA.length)
-        // console.log(americanIPA[0]);
+ d3.csv(url,function (data){
 
-     // var highABV= data.filter(function (d){
+   //https://stackoverflow.com/questions/30874617/d3-max-didnt-get-the-correct-value
+   data.forEach(function(d) {
+     //Need to parse float so that the max function doesn't order based on string value
+        d[dimension] = parseFloat(d[dimension]);
+    });
+   var m=d3.min(data,function (d){
+      return d[dimension];
+    });
 
-     //     return (d.abv>=0.05)
-     // });
+    console.log(m);
+    UpdateOutput(m);
+ });
+}
 
-     // console.log(" # high ABV Beers:  " +highABV.length)
-     // console.log(highABV[0]);
+function FindMean(){
+ console.log("It's Working (Mean)");
 
-     // var countAle=0;
+ d3.csv(url,function (data){
 
-     // data.forEach(function (d){
+   //https://stackoverflow.com/questions/30874617/d3-max-didnt-get-the-correct-value
+   data.forEach(function(d) {
+     //Need to parse float so that the max function doesn't order based on string value
+        d[dimension] = parseFloat(d[dimension]);
+    });
+   var m=d3.mean(data,function (d){
+      return d[dimension];
+    });
 
-     //     if (d.name.includes("Ale")){
-     //         countAle++;
-     //     }
-
-     // })
-
-
-     console.log(" The number of Ale Beers is "+countAle);
-
-
-
-     // var minPixel=d3.min(data,function (d){
-     //         return d['Effective pixels'];
-     //     });
-     //
-     //     var maxDimensions=d3.max(data,function (d){
-     //
-     //         return d['Dimensions'];
-     //
-     //
-     //     })
-     //
-     //
-     //
-     //
-     //      console.log("  min effectie pixel . " +minPixel+"  and  max dimension "+maxDimensions) ;
-
-
-
-   });
-
-
-
-
-   //Updated text fields for diplaying selected dimensions and outputs of queries
-   var selectedText = document.getElementById("SelectedText");
-   var output = document.getElementById("Output");
-
-   //Selected Dimension for use with d3 queries
-   var dimension = "";
-
-
-   function UpdateSelected(update, name){
-     selectedText.innerHTML = update;
-     dimension = name;
-   }
-
-   function UpdateOutput(info){
-     output.innerHTML = info;
-   }
-
-   //Button listeners for each of the selectable dimensions
-   document.getElementById("Selector-ID").addEventListener("click", function(){
-    UpdateSelected("ID", "id");
-   });
-   document.getElementById("Selector-ABV").addEventListener("click", function(){
-    UpdateSelected("ABV", "abv");
-   });
-   document.getElementById("Selector-IBU").addEventListener("click", function(){
-    UpdateSelected("IBU", "ibu");
-   });
-   document.getElementById("Selector-Name").addEventListener("click", function(){
-    UpdateSelected("Name", "name");
-   });
-   document.getElementById("Selector-Style").addEventListener("click", function(){
-    UpdateSelected("Style", "style");
-   });
-   document.getElementById("Selector-Brewery").addEventListener("click", function(){
-    UpdateSelected("Brewery", "brewery_id");
-   });
-   document.getElementById("Selector-Ounces").addEventListener("click", function(){
-    UpdateSelected("Ounces", "ounces");
-   });
-
-
-
-   function FindMax(){
-     console.log("It's Working (Max)");
-
-     d3.csv(url,function (data){
-
-       //https://stackoverflow.com/questions/30874617/d3-max-didnt-get-the-correct-value
-       data.forEach(function(d) {
-         //Need to parse float so that the max function doesn't order based on string value
-            d[dimension] = parseFloat(d[dimension]);
-        });
-       var m=d3.max(data,function (d){
-          return d[dimension];
-        });
-
-        console.log(m);
-        UpdateOutput(m);
-     });
-   }
-
-   function FindMin(){
-     console.log("It's Working (Min)");
-
-     d3.csv(url,function (data){
-
-       //https://stackoverflow.com/questions/30874617/d3-max-didnt-get-the-correct-value
-       data.forEach(function(d) {
-         //Need to parse float so that the max function doesn't order based on string value
-            d[dimension] = parseFloat(d[dimension]);
-        });
-       var m=d3.min(data,function (d){
-          return d[dimension];
-        });
-
-        console.log(m);
-        UpdateOutput(m);
-     });
-   }
-
-   function FindMean(){
-     console.log("It's Working (Mean)");
-
-     d3.csv(url,function (data){
-
-       //https://stackoverflow.com/questions/30874617/d3-max-didnt-get-the-correct-value
-       data.forEach(function(d) {
-         //Need to parse float so that the max function doesn't order based on string value
-            d[dimension] = parseFloat(d[dimension]);
-        });
-       var m=d3.mean(data,function (d){
-          return d[dimension];
-        });
-
-        console.log(m);
-        UpdateOutput(m);
-     });
-   }
+    console.log(m);
+    UpdateOutput(m);
+ });
+}
